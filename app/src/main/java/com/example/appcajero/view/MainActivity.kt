3ac6fe95f991facade7b80ff9760f3d4953c8a6e
Andrew Dianderas Apaza <andrewdiapa09@gmail.com>
2022-09-19 00:10:16 -0500

    cambie el texto2s

--- app/src/main/java/com/example/appcajero/view/MainActivity.kt
+++ app/src/main/java/com/example/appcajero/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appcajero.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.appcajero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var numero: Int
+        var numero2: Int
+        var suma :Int
+
+        binding.btnSumar.setOnClickListener {
+            numero = binding.nro1.text.toString().toInt()
+            numero2 = binding.num1.text.toString().toInt()
+            suma = numero + numero2
+
+            binding.sum.text = "la suma de lo que ingresate es $suma"
+
+            //Toast.makeText(this,"la suma de lo que ingresate es $suma",Toast.LENGTH_LONG).show()
+        }
     }
 }
